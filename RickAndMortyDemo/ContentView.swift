7b30bc3b4aca814af4a6bd69557870ae2a9b1a84
XING <zhaoxing8325@gmail.com>
2022-01-04 22:22:23 -0800

    Initial Commit

---
+++ RickAndMortyDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RickAndMortyDemo
+//
+//  Created by Xing Zhao on 2022/1/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RickAndMortyDemo/RickAndMortyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyDemoApp.swift
+//  RickAndMortyDemo
+//
+//  Created by Xing Zhao on 2022/1/4.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
